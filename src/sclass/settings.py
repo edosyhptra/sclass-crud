6e0796692998667f4e80af002e3d9b19cfee2dd3
edosyhptra <edo23syahputra@gmail.com>
2025-04-25 11:13:45 +0700

    init postgres

--- src/sclass/settings.py
+++ src/sclass/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'sclass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sclass2', # nama database
+        'USER': 'edo', # username database
+        'PASSWORD': 'edo',
+        'PORT': '5432',
+        'HOST': 'localhost', # host database
     }
 }
 
